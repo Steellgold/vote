817449337148cb568c42b23562dd81a93f991e0f
Gaëtan H <gaetanhus@gmail.com>
2025-01-07 19:41:01 +0100

    refactor(route.ts): simplify GET function parameters by removing req argument to streamline code and improve readability

--- app/api/poll/[poll]/route.ts
+++ app/api/poll/[poll]/route.ts
@@ -7,7 +7,7 @@ type Params = {
   }
 }
 
-export const GET = async (req: NextRequest, { params }: Params): Promise<NextResponse> => {
+export const GET = async ({ params }: Params): Promise<NextResponse> => {
   try {
     const poll = await prisma.poll.findFirst({
       where: {
