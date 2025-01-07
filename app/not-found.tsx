6d0433b27528acec41a6efa4d880931f192f133c
Gaëtan H <gaetanhus@gmail.com>
2025-01-07 19:07:46 +0100

    feat(not-found.tsx): enhance NotFound component to accept pollNotFound prop for better user feedback style(not-found.tsx): clean up formatting and improve readability of the message displayed when a poll is not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,21 @@
 import { buttonVariants } from "@/components/ui/button";
+import { Component } from "@/lib/types";
 import Link from "next/link";
 
-const NotFound = () => {
+type NotFoundProps = {
+  pollNotFound?: boolean;
+};
+
+const NotFound: Component<NotFoundProps> = ({ pollNotFound = false }) => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center relative">
-      <h2 className="text-3xl font-bold">
-        You&apos;re lost?
-      </h2>
+      <h2 className="text-3xl font-bold">You&apos;re lost?</h2>
 
       <p className="mb-4">
-        The page you are looking for does not exist.
+        {pollNotFound
+          ? "Hmm, we couldn't find the poll you're looking for."
+          : "The page you are looking for does not exist."
+        }
       </p>
 
       <Link href="/" className={buttonVariants({ variant: "outline" })}>
